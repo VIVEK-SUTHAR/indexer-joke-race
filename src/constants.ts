1a3c73c3ed7625e311ebfeb878f5bdf54c15a44a
Vivek Suthar <sutharvivek18@gmail.com>
2024-11-15 09:14:59 +0700

    rate limits

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 import { clusterApiUrl, PublicKey } from "@solana/web3.js";
 
 export const PROGRAM_ID = new PublicKey(
-  "7AM2gAnqJ4B3NGJbSrfVW6LBGYzoph3XEhEFtWZ2n5Z",
+  "CGfVb6h6wHnAxLfMw3EnssxBEPQgdyNSQzXtWVYkx7Vn",
 );
 
-export const SOLANA_RPC_URL = "https://staging-rpc.dev2.eclipsenetwork.xyz";
+export const SOLANA_RPC_URL = "";
